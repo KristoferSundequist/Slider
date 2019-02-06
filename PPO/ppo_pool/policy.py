c528c9d31c86e42b07b90539cb6475edcecff972
Kristofer Sundequist <krajle@gmail.com>
2019-02-06 10:33:48 +0100

    refactores ppo_pool

--- PPO/ppo_pool/policy.py
+++ PPO/ppo_pool/policy.py
@@ -2,7 +2,6 @@ import torch
 from torch import autograd, nn, optim
 import torch.nn.functional as F
 import numpy as np
-from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 class policy(nn.Module):
     def __init__(self):
@@ -19,7 +18,6 @@ class policy(nn.Module):
         self.value_out = nn.Linear(200,1)
         
         self.apply(self.weight_init)
-        self.opt = optim.Adam(self.parameters(), lr=1e-03, eps=1e-05)
 
     def weight_init(self, m):
         if isinstance(m, nn.Linear):
@@ -36,7 +34,6 @@ class policy(nn.Module):
         self.load_state_dict(torch.load(name))
         
     def forward(self, state):
-        
         a = F.relu(self.ainp(state))
         a = F.relu(self.a0(a))
         a = F.relu(self.a1(a))
@@ -49,76 +46,7 @@ class policy(nn.Module):
         
         return actions, value
 
-    def train(self,states,old_actionprobs,actions,values,advantages,beta,ppo_epochs,eps,learning_rate,batch_size, use_critic):
-        self.opt.learning_rate = learning_rate
-        states = torch.from_numpy(states).float()
-        actions = torch.from_numpy(actions).float()
-        advantages = torch.from_numpy(advantages).float()
-        old_actionprobs = old_actionprobs.float()
-
-        
-        returns = values+advantages
-        normalized_advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-08)
-        log_action_loss = log_value_loss = log_entropy_loss = 0
-    
-        for _ in range(ppo_epochs):
-            sampler = BatchSampler(SubsetRandomSampler(range(len(states))), batch_size,drop_last=False)
-            for indices in sampler:
-                indices = torch.LongTensor(indices)
-                taken_actions = actions[indices]
-                
-                new_actionprobs, new_values = self.forward(states[indices])
-                new_action = torch.sum(new_actionprobs*taken_actions,1)
-                
-                old_actionprobs_batch = old_actionprobs[indices]
-                old_action_batch = torch.sum(old_actionprobs_batch*taken_actions,1)
-                
-                entropy = -torch.sum(new_actionprobs * torch.log(new_actionprobs + 1e-08),1)
-                
-                ratio = new_action/old_action_batch
-
-                if use_critic:
-                    adv = normalized_advantages[indices]
-                else:
-                    adv = normalized_returns[indices]
-                    
-                surr1 = ratio*adv
-                surr2 = torch.clamp(ratio, 1 - eps , 1 + eps)*adv
-                action_loss = -torch.min(surr1,surr2).mean()
-
-                if use_critic:
-                    old_vals = values[indices]
-                    
-                    value_target = returns[indices]
-                    value_loss1 = (new_values.view(-1) - value_target).pow(2)
-                
-                    clipped = old_vals + torch.clamp(new_values.view(-1) - old_vals, -eps, eps)
-                    value_loss2 = (clipped - value_target).pow(2)
-                
-                    value_loss = torch.max(value_loss1, value_loss2).mean()
-                    
-                    loss = action_loss + .5*value_loss - beta*entropy.mean()
-                else:
-                    value_loss = torch.tensor([0])
-                    loss = action_loss - beta*entropy.mean()
-
-
-                log_action_loss += action_loss
-                log_value_loss += value_loss
-                log_entropy_loss += entropy.mean()
-
-                self.zero_grad()
-                loss.backward()
-                self.opt.step()
-
-        num_updates = ppo_epochs + int(len(states)/batch_size)
-        return (log_action_loss/num_updates).item(),(log_value_loss/num_updates).item(),(log_entropy_loss/num_updates).item()
-
     def get_action(self, state):
         with torch.no_grad():
             out,value = self.forward(torch.from_numpy(state).view(1,8).float())
             return out, out.multinomial(1).item(), value
-
-
-
-
--- PPO/ppo_pool/slider.py
+++ PPO/ppo_pool/slider.py
@@ -1,40 +1,26 @@
-from graphics import *
 import numpy as np
-import policy
-import torch
-import time
-from multiprocessing import Pool, cpu_count
-
-ncpus = cpu_count()
+from graphics import *
 
 ##########
 ## GAME ##
 ##########
 
-width = 700
-height = 700
-win = GraphWin("canvas", width,height)
-win.setBackground('lightskyblue')
-    
-def clear(win):
-    for item in win.items[:]:
-        item.undraw()
-    win.update()
-
 class Slider:
     
-    def __init__(self):
+    def __init__(self,width, height):
         self.x = np.random.randint(50,width-50)
         self.y = np.random.randint(50,height-50)
         self.radius = 30
         self.dx = np.random.randint(-10,10)
         self.dy = np.random.randint(-10,10)
         self.dd = 0.5
+        self.width = width
+        self.height = height
 
 
     def reset(self):
-        self.x = np.random.randint(50,width-50)
-        self.y = np.random.randint(50,height-50)
+        self.x = np.random.randint(50,self.width-50)
+        self.y = np.random.randint(50,self.height-50)
         self.radius = 30
         self.dx = np.random.randint(-10,10)
         self.dy = np.random.randint(-10,10)
@@ -60,16 +46,16 @@ class Slider:
             
     def update(self):
 
-        if self.x + self.radius >= width:
-            self.x = width - self.radius
+        if self.x + self.radius >= self.width:
+            self.x = self.width - self.radius
             self.dx *= -1
                     
         if self.x - self.radius <= 0:
             self.x = self.radius
             self.dx *= -1
                 
-        if self.y + self.radius >= height:
-            self.y = height - self.radius
+        if self.y + self.radius >= self.height:
+            self.y = self.height - self.radius
             self.dy *= -1
 
         if self.y - self.radius <= 0:
@@ -88,14 +74,16 @@ class Slider:
         c.draw(win)
 
 class Target:
-    def __init__(self,radius):
+    def __init__(self,radius,width, height):
         self.x = np.random.randint(width)
         self.y = np.random.randint(height)
         self.radius = radius
+        self.width = width
+        self.height = height
 
     def reset(self):
-        self.x = np.random.randint(width)
-        self.y = np.random.randint(height)
+        self.x = np.random.randint(self.width)
+        self.y = np.random.randint(self.height)
 
     def render(self,win):
         c = Circle(Point(self.x, self.y), self.radius)
@@ -115,14 +103,17 @@ class Target:
             self.y += 1
 
 class Enemy:
-    def __init__(self,radius):
+    def __init__(self,radius,width, height):
         self.x = np.random.randint(width)
         self.y = np.random.randint(height)
         self.radius = radius
+        self.width = width
+        self.height = height
+    
 
     def reset(self):
-        self.x = np.random.randint(width)
-        self.y = np.random.randint(height)
+        self.x = np.random.randint(self.width)
+        self.y = np.random.randint(self.height)
 
     def render(self,win):
         c = Circle(Point(self.x, self.y), self.radius)
@@ -140,160 +131,42 @@ class Enemy:
             self.y -= 1
         else:
             self.y += 1
-               
-def intersect(slider, target):
-    return slider.radius + target.radius > np.sqrt(np.power(slider.x - target.x, 2) + np.power(slider.y - target.y, 2))
 
-
-s = Slider()
-t = Target(50)
-enemy = Enemy(30)
-
-##############
-## RL STUFF ##
-##############
-
-state_space_size = 8
-agent = policy.policy()
-
-def get_state():
-    #return np.array([s.x/width, s.y/height, s.dx/10, s.dy/10, t.x/width, t.y/height, 0,0])
-    return np.array([s.x/width, s.y/height, s.dx/10, s.dy/10, t.x/width, t.y/height, enemy.x/width, enemy.y/height])
-
-def step(action):
-    s.push(action)
-    s.update()
-    enemy.update(s.x,s.y)
+class Game:
     
-    reward = 0
-    if intersect(s,t):
-        reward += .2
-        t.reset()
-        
-    if intersect(s,enemy):
-        reward -= 1
-        enemy.reset()
-        
-    return reward, get_state()
-    
-def getEpisode(n,agent):
-    t.reset()
-    s.reset()
-    enemy.reset()
-    
-    states = np.zeros((n, state_space_size))
-    actionprobs = []
-    actions = np.zeros((n,4))
-    values = []
-    rewards = np.zeros(n)
-    
-    current_state = get_state()
-    
-    for i in range(n):
-        actionprob, action, value = agent.get_action(current_state)
+    def __init__(self, width, height):
+        self.s = Slider(width, height)
+        self.t = Target(50, width, height)
+        self.enemy = Enemy(30,width, height)
+        self.state_space_size = 8
+        self.action_space_size = 4
+        self.width = width
+        self.height = height
 
-        states[i] = current_state
-        actions[i] = np.eye(4)[action]
-        actionprobs.append(actionprob)
-        values.append(value)
-        
-        reward, current_state = step(action)
+    def intersect(self, slider, target):
+        return slider.radius + target.radius > np.sqrt(np.power(slider.x - target.x, 2) + np.power(slider.y - target.y, 2))
 
-        rewards[i] = reward
+    def get_state(self):
+        return np.array([self.s.x/self.width, self.s.y/self.height, self.s.dx/10, self.s.dy/10, self.t.x/self.width, \
+            self.t.y/self.height, self.enemy.x/self.width, self.enemy.y/self.height])
 
-    actionprobs = torch.cat(actionprobs).float().data
-    values = torch.cat(values).view(-1).float().data
-    return states, actionprobs, actions, values, rewards
+    def render(self, win):
+        self.t.render(win)
+        self.s.render(win)
+        self.enemy.render(win)
 
-#torch.save(policy.agent.state_dict(), PATH)
-#policy.agent.load_state_dict(torch.load(PATH))
+    def step(self, action):
+        self.s.push(action)
+        self.s.update()
+        self.enemy.update(self.s.x,self.s.y)
 
+        reward = 0
+        if self.intersect(self.s, self.t):
+            reward += .2
+            self.t.reset()
 
-running_reward = None
-# export OMP_NUM_THREADS=1
-#train(20,3000,10,0.01,4,0.1,0.99,0.95,0.0001,4000, True, 2)
-def train(num_actors,episode_size,episodes,beta,ppo_epochs,eps,gamma,lambd,lr,batch_size, use_critic, num_workers):
-    time_start = time.time()
-    global running_reward
-    decay = 0.9
-    
-    torch.set_num_threads(1)
-    pool = Pool(ncpus)
-    torch.set_num_threads(ncpus)
-    
-    for i in range(episodes):
-        tact = time.time()
-        
-        data = pool.starmap(getEpisode, [(episode_size,agent) for _ in range(num_actors)])
-        
-        data = list(zip(*data))
-        states_data = list(data[0])
-        actionprobs_data = list(data[1])
-        actions_data = list(data[2])
-        values_data = list(data[3])
-        rewards = list(data[4])
-        advantage_data = [generalized_advantage_estimation(rewards[i],values_data[i].numpy(),gamma,lambd,values_data[i][-1]) \
-                          for i in range(num_actors)]
+        if self.intersect(self.s,self.enemy):
+            reward -= 1
+            self.enemy.reset()
 
-        
-        episode_reward_sums = np.array(rewards).sum(1)
-        meanreward = episode_reward_sums.mean()
-        if running_reward == None:
-            running_reward = meanreward
-        else:
-            running_reward = decay*running_reward + (1-decay)*meanreward
-            
-        
-        acc_states = np.concatenate(states_data)
-        acc_actionprobs = torch.cat(actionprobs_data)
-        acc_actions = np.concatenate(actions_data)
-        acc_values = torch.cat(values_data)
-        acc_advantages = np.concatenate(advantage_data)
-        
-        print("actTime: ", time.time()-tact)
-        ttrain = time.time()
-        a,v,e = agent.train(acc_states, acc_actionprobs, acc_actions, acc_values, acc_advantages,beta,ppo_epochs,eps,lr,batch_size, use_critic)
-        print("trainTime: ", time.time()-ttrain)
-        #print(i+1, "/", episodes, running_reward, " ", meanreward, "Losses (action, value, entropy): ", a, v, e)
-        print("Episode: ", i+1, "/", episodes, " Rewards: ", running_reward, " ", meanreward)
-    print("Time: ", time.time()-time_start)
-    pool.close()
-    pool.join()
-    
-def generalized_advantage_estimation(rewards,values,gamma,lambd,last):
-    rewards = rewards.astype(float)
-    values = np.append(values,last)
-    gaes = np.zeros_like(rewards)
-    gae = 0
-    for i in reversed(range(len(gaes))):
-        delta = rewards[i] + gamma*values[i+1] - values[i]
-        gae = delta + gamma * lambd * gae
-        gaes[i] = gae
-
-    return gaes
-        
-def discount(r,gamma,init):
-    dr = np.zeros_like(r)
-    dr[dr.shape[0]-1] = init
-    R = init
-    for i in reversed(range(len(r))):
-        R = r[i] + gamma*R
-        dr[i] = R
-
-    return dr
-
-def agent_loop(iterations):
-    t.reset()
-    s.reset()
-    enemy.reset()
-    import time
-    for i in range(iterations):
-            _,action,v = agent.get_action(get_state())
-            _,_ = step(action)
-                
-            t.render(win)
-            s.render(win)
-            enemy.render(win)
-            Text(Point(100,100), v.data[0][0]).draw(win)
-            time.sleep(0.01)
-            clear(win)
+        return reward, self.get_state()

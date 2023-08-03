fix muzero generalized advantage estimation calculation

CatNet/slider.py
@@ -98,6 +98,7 @@ class Enemy:
         self.x = np.random.randint(globals.width)
         self.y = np.random.randint(globals.height)
         self.radius = radius
+        self.speed = 3
 
     def reset(self):
         self.x = np.random.randint(globals.width)
@@ -111,14 +112,14 @@ class Enemy:
 
     def update(self, sliderx, slidery):
         if self.x > sliderx:
-            self.x -= 1
+            self.x -= self.speed
         else:
-            self.x += 1
+            self.x += self.speed
 
         if self.y > slidery:
-            self.y -= 1
+            self.y -= self.speed
         else:
-            self.y += 1
+            self.y += self.speed
 
 
 class Game():

MuZero/get_data.py
@@ -4,6 +4,8 @@ import torch
 import time
 import copy
 from multiprocessing import Pool, cpu_count
+import random
+import scipy
 
 import slider
 from policy import *
@@ -14,15 +16,17 @@ ncpus = cpu_count()
 
 
 def get_episode(
-        num_initial_states: int,
-        max_iters: int,
-        representation: Representation,
-        dynamics: Dynamics,
-        prediction: Prediction,
-        temperature: float,
-        game,
-        discount: float):
+    num_initial_states: int,
+    max_iters: int,
+    representation: Representation,
+    dynamics: Dynamics,
+    prediction: Prediction,
+    temperature: float,
+    game,
+    discount: float
+):
 
+    scipy.random.seed()
     episode = Episode(game.state_space_size)
 
     initial_states = [np.zeros(game.state_space_size)
@@ -34,36 +38,42 @@ def get_episode(
         initial_states.pop(0)
         initial_states.append(state)
 
-        root = MCTS(initial_states, representation, dynamics, prediction, game.action_space_size, 50, discount)
+        root = MCTS(initial_states, representation, dynamics,
+                    prediction, game.action_space_size, 100, discount)
 
         action = sample_action(root, temperature)
         reward, _ = game.step(action)
 
-        episode.add_transition(reward, action, state, root.get_search_policy(), root.search_value())
+        episode.add_transition(reward, action, state,
+                               root.get_search_policy(), root.search_value())
 
     episode.calc_targets_gae(discount)
     return episode
 
 
 def get_episodes(
-        n_episodes: int,
-        num_initial_states: int,
-        max_iters: int,
-        representation: Representation,
-        dynamics: Dynamics,
-        prediction: Prediction,
-        temperature: float,
-        gameFactory,
-        discount):
+    n_episodes: int,
+    num_initial_states: int,
+    max_iters: int,
+    representation: Representation,
+    dynamics: Dynamics,
+    prediction: Prediction,
+    temperature: float,
+    gameFactory,
+    discount
+):
 
     torch.set_num_threads(1)
-    pool = Pool(ncpus)
 
-    episodes = pool.starmap(get_episode,
-                            [(num_initial_states, max_iters, representation, dynamics, prediction, temperature, gameFactory(), discount) for _ in range(n_episodes)])
+    with Pool(ncpus) as pool:
+        episodes = pool.starmap(
+            get_episode,
+            [(num_initial_states, max_iters, representation, dynamics, prediction,
+              temperature, gameFactory(), discount) for _ in range(n_episodes)]
+        )
 
     torch.set_num_threads(ncpus)
-    
+
     return episodes
 
 
@@ -81,12 +91,14 @@ def test_get_episode():
     state_space_size = game.state_space_size
     inner_size = 30
     action_space_size = game.action_space_size
-    representation = Representation(num_initial_states, state_space_size, inner_size)
+    representation = Representation(
+        num_initial_states, state_space_size, inner_size)
     dynamics = Dynamics(inner_size, action_space_size)
     prediction = Prediction(inner_size, action_space_size)
 
     num_iterations = 30
-    e = get_episode(num_initial_states, num_iterations, representation, dynamics, prediction, 1, game, 0.99)
+    e = get_episode(num_initial_states, num_iterations,
+                    representation, dynamics, prediction, 1, game, 0.99)
 
     assert len(e._states) == num_iterations
 
@@ -105,7 +117,8 @@ def test_get_episodeS():
     state_space_size = game.state_space_size
     inner_size = 30
     action_space_size = game.action_space_size
-    representation = Representation(num_initial_states, state_space_size, inner_size)
+    representation = Representation(
+        num_initial_states, state_space_size, inner_size)
     dynamics = Dynamics(inner_size, action_space_size)
     prediction = Prediction(inner_size, action_space_size)
 
@@ -117,4 +130,5 @@ def test_get_episodeS():
     assert len(episodes) == num_episodes
     assert episodes[0].get_num_transitions() == num_iterations
 
-    assert not np.array_equal(episodes[0]._states[0], episodes[1]._states[0])  # make sure not same data in diff episodes
+    # make sure not same data in diff episodes
+    assert not np.array_equal(episodes[0]._states[0], episodes[1]._states[0])

MuZero/main.py
@@ -59,7 +59,7 @@ simpredictor = Projector(inner_size)
 simpredictor_optimizer = torch.optim.AdamW(
     simpredictor.parameters(), lr=3e-4, weight_decay=1e-4)
 
-replay_buffer = Replay_buffer(100)
+replay_buffer = Replay_buffer(6*10)
 #replay_buffer = load_from_file('trajectories/trajs20230628')
 
 def save(name):

MuZero/policy.py
@@ -32,8 +32,7 @@ class Representation(nn.Module):
 
         self.w1 = nn.Linear(num_states * state_space_size, 256)
         self.w2 = nn.Linear(256, 256)
-        self.w3 = nn.Linear(256, 256)
-        self.w4 = nn.Linear(256, inner_size)
+        self.w3 = nn.Linear(256, inner_size)
 
     def prepare_states(self, initial_states):
         return torch.Tensor(np.reshape(np.array(initial_states), [-1, self.num_states*self.state_space_size]))
@@ -41,8 +40,7 @@ class Representation(nn.Module):
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         x = F.relu(self.w1(input))
         x = F.relu(self.w2(x))
-        x = F.relu(self.w3(x))
-        out = torch.tanh(self.w4(x))
+        out = torch.tanh(self.w3(x))
         return out
 
 

MuZero/reanalyze.py
@@ -1,9 +1,7 @@
 import numpy as np
-import policy
 import torch
-import time
-import copy
 from multiprocessing import Pool, cpu_count
+import scipy
 
 import slider
 from policy import *
@@ -22,13 +20,17 @@ def reanalyze_episode(
         num_initial_states: int,
         action_space_size: int,
         num_unroll_steps: int):
+    
+    scipy.random.seed()
 
     for i in range(episode.get_num_transitions()):
         initial_states = episode.gather_initial_state(i, num_initial_states, 0)
 
-        root = MCTS(initial_states, representation, dynamics, prediction, action_space_size, 20, discount)
+        root = MCTS(initial_states, representation, dynamics,
+                    prediction, action_space_size, 50, discount)
 
-        episode.update_value_and_policy(i, root.get_search_policy(), root.search_value())
+        episode.update_value_and_policy(
+            i, root.get_search_policy(), root.search_value())
 
     episode.calc_targets_gae(discount)
     return episode
@@ -45,13 +47,15 @@ def reanalze_episodes(
         num_unroll_steps: int):
 
     torch.set_num_threads(1)
-    pool = Pool(ncpus)
-
-    reanalyzed_episodes = pool.starmap(reanalyze_episode,
-                            [(episode, representation, dynamics, prediction, discount, num_initial_states, action_space_size, num_unroll_steps) for episode in episodes])
+    with Pool(ncpus) as pool:
+        reanalyzed_episodes = pool.starmap(
+            reanalyze_episode,
+            [(episode, representation, dynamics, prediction, discount, num_initial_states,
+              action_space_size, num_unroll_steps) for episode in episodes]
+        )
 
     torch.set_num_threads(ncpus)
-    
+
     return reanalyzed_episodes
 
 
@@ -61,5 +65,6 @@ TEST test_something()
 
 '''
 
+
 def test_something():
-    assert(5 == 5)
+    assert (5 == 5)

PPO/train.py
@@ -9,8 +9,8 @@ from slider import *
 from torch.utils.data.sampler import BatchSampler, SubsetRandomSampler
 
 ncpus = cpu_count()
-width = 1500
-height = 1000
+width = 800
+height = 700
 agent = policy.policy(Game.state_space_size, Game.action_space_size)
 optimizer = torch.optim.Adam(agent.parameters(), lr=3e-4, eps=1e-5)
 